2e1bbcb21f42333dd710e2077d25ffc94054fb89
joshknt <joshknt@gmail.com>
2017-04-10 20:07:12 -0500

    Showing Default Page

--- main.go
+++ main.go
@@ -3,18 +3,34 @@ package main
 import (
 	//accounts "455/Accounts"
 	//courses "455/Courses"
-	"fmt"
-	//"github.com/gorilla/mux"
-	//"log"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
-func sayhello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Josh you fuck")
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("adviseUI.html")
+	t.Execute(w, p)
 }
 
 func main() {
-	http.HandleFunc("/", sayhello)
+	http.HandleFunc("/", defaultHandler)
 	http.ListenAndServe(":9090", nil)
 
 }
