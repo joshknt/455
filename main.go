f302cfeffe9c4886263b58ec652a42bb220b1095
joshknt <joshknt@gmail.com>
2017-04-11 16:27:52 -0500

    Added HTML files

--- main.go
+++ main.go
@@ -23,22 +23,34 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func defaultViewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/"):]
 	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("adviseUI.html")
+	t, _ := template.ParseFiles("DefaultView.html")
+	t.Execute(w, p)
+}
+
+func adminViewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("AdminView.html")
 	t.Execute(w, p)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+	//un := r.Form["username"]
+	//pass := r.Form["password"]
 	fmt.Println("UN: ", r.Form["username"])
 	fmt.Println("Pass: ", r.Form["password"])
+
+	http.Redirect(w, r, "/login", 302)
 }
 
 func main() {
-	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/", defaultViewHandler)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/admin", adminViewHandler)
 	http.ListenAndServe(":9090", nil)
 
 }
