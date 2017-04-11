c9fb34b196ea2891f44b48845a8075b0438fa990
acaballero9085 <acaballero9085@gmail.com>
2017-04-11 15:13:38 -0500

    Minor change in GetArea3, and ValidateArea2

--- Courses/courses.go
+++ Courses/courses.go
@@ -39,7 +39,7 @@ func GetArea2(area2 *[32]Course) {
 
 // GetArea3 : Grabs Area 3 from the database and fills the array being passed
 // Author: Arturo Caballero
-func GetArea3(area3 *[20]Course) {
+func GetArea3(area3 *[18]Course) {
 	var i int = 0
 
 	// will eventually be replaced by database calls
@@ -123,12 +123,166 @@ func ValidateArea2(area2 *[32]Course) bool {
 	for range a3 {
 		if a3[i].Completed == true {
 			s2 = true
+			break
 		}
 		i++
 	}
 
 	// if all 3 areas are valid return true, else false
 	if a1[0].Completed && s1 && s2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// ValidateArea3 : checks area3 to see if requirements are fulfilled, returns boolean
+// Author : Arturo Caballero
+func ValidateArea3(area3 *[18]Course) bool {
+	// split the array into 2 slices
+	var a1 []Course = area3[0:8]
+	var a2 []Course = area3[8:18]
+
+	// boolean variables for sequences and incrementor
+	var s1, s2 bool
+	var i int = 0
+
+	// check first slice, if 1 class is completed make s1 true
+	for range a1 {
+		if a1[i].Completed == true {
+			s1 = true
+			break
+		}
+		i++
+	}
+
+	// check 2nd slice if any sequence is valid
+	if a2[0].Completed && a2[1].Completed {
+		s2 = true
+	} else if a2[2].Completed && a2[3].Completed {
+		s2 = true
+	} else if a2[4].Completed && a2[5].Completed {
+		s2 = true
+	} else if a2[6].Completed && a2[7].Completed {
+		s2 = true
+	}
+
+	// if s1 and s2 true than area3 is valid
+	if s1 && s2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// ValidateArea4 : checks area4 to see if requirements are fulfilled, return boolean
+// Author : Arturo Caballero
+func ValidateArea4(area4 *[13]Course) bool {
+	// split the array into 2 slices
+	var a1 []Course = area4[0:4]
+	var a2 []Course = area4[4:13]
+
+	// boolean variables for sequences, incrementor, and counter
+	var count, i int = 0, 0
+	var s1, s2 bool
+
+	// check 1st slice if any sequence is valid
+	if a1[0].Completed && a1[1].Completed {
+		s1 = true
+	} else if a1[2].Completed && a1[3].Completed {
+		s1 = true
+	}
+
+	// check 2nd slice, if at least 2 classes are completed s2 changed to true
+	for range a2 {
+		if a2[i].Completed == true {
+			count++
+		}
+		if count == 2 {
+			s2 = true
+			break
+		}
+		i++
+	}
+
+	if s1 && s2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// ValidateMajor : checks major to see if requirements are fulfilled, returns boolean
+// Author : Arturo Caballero
+func ValidateMajor(major *[31]Course) bool {
+	// split the array into 5 slices
+	var m1 []Course = major[0:5]
+	var m2 []Course = major[5:14]
+	var m3 []Course = major[14:19]
+	var m4 []Course = major[19:24]
+	var m5 []Course = major[24:31]
+
+	var s1, s2, s3, s4, s5 bool
+	var i, count int = 0, 0
+
+	// check first slice, if all classes completed make s1 true
+	for range m1 {
+		if m1[i].Completed == true {
+			count++
+		}
+		if count >= 5 {
+			s1 = true
+		}
+		i++
+	}
+
+	count, i = 0, 0
+	// check 2nd slice, if all classes completed make s2 true
+	for range m2 {
+		if m2[i].Completed == true {
+			count++
+		}
+		if count >= 9 {
+			s2 = true
+		}
+		i++
+	}
+
+	i = 0
+	// check 3rd slice, if 1 class is completed make s3 true
+	for range m3 {
+		if m3[i].Completed == true {
+			s3 = true
+			break
+		}
+		i++
+	}
+
+	count, i = 0, 0
+	// check 4th slice, if 3 classes are completed make s4 true
+	for range m4 {
+		if m4[i].Completed == true {
+			count++
+		}
+		if count >= 3 {
+			s4 = true
+			break
+		}
+		i++
+	}
+
+	i = 0
+	// check 5th slice, if 1 class is completed make s5 true
+	for range m5 {
+		if m5[i].Completed == true {
+			s5 = true
+			break
+		}
+		i++
+	}
+
+	// if all sequences are valid return true
+	if s1 && s2 && s3 && s4 && s5 {
 		return true
 	} else {
 		return false
