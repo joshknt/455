b883bc3fff320504a3a47b08dc5bbbc1c8a70dec
joshknt <joshknt@gmail.com>
2017-04-22 15:53:46 -0500

    Working Class requests

--- main.go
+++ main.go
@@ -11,13 +11,23 @@ import (
 	"net/http"
 )
 
+var allCourses [3]courses.Course
+
+func init() {
+	allCourses[0] = courses.Course{Hours: 3, Grade: "A", DepartmentID: "CS",
+		Name: "155", Completed: true}
+
+	allCourses[1] = courses.Course{Hours: 3, Grade: "B", DepartmentID: "CS",
+		Name: "255", Completed: true}
+
+	allCourses[2] = courses.Course{Hours: 3, Grade: "D", DepartmentID: "CS",
+		Name: "310", Completed: false}
+}
+
 type page struct {
 	Title string
 	Body  []byte
 }
-
-var c1 = courses.Course{Hours: 3, Grade: "A", DepartmentID: "CS",
-	Name: "155", Completed: true}
 
 func loadPage(title string) (*page, error) {
 	filename := title + ".txt"
@@ -43,22 +53,37 @@ func adminViewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	//un := r.Form["username"] //doesn't work,  []string != string
-	//pass := r.Form["password"]
-	fmt.Println("UN: ", r.Form["username"])
-	fmt.Println("Pass: ", r.Form["password"])
-
-	adminViewHandler(w, r)
+	user := r.URL.Query()["username"]
+	pass := r.URL.Query()["password"]
+	fmt.Println("UN: ", user)
+	fmt.Println("Pass: ", pass)
+	if user != nil || pass != nil {
+		fmt.Println("UN: ", user)
+		fmt.Println("Pass: ", pass)
+		//if password matches users pass in db
+		adminViewHandler(w, r)
+	} else {
+		http.Redirect(w, r, "/", 302)
+	}
 }
 
 func getCourses(w http.ResponseWriter, r *http.Request) {
+	choice := r.URL.Query()["choice"]
+	degree := r.URL.Query()["degrees"]
+	fmt.Println(choice)
+	fmt.Println(degree)
+
+	for i := range allCourses {
+		json.NewEncoder(w).Encode(allCourses[i])
+	}
 
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", defaultViewHandler)
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/getcourses", getCourses).Methods("GET")
 
 	http.ListenAndServe(":9090", router)
 }
